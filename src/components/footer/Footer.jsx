3aee61cf462a61045f0ea204abb2752b865c9723
Muro Bedianidze <123646281+murobedianidze@users.noreply.github.com>
2024-03-15 23:14:29 +0400

    changed

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,14 +8,14 @@ const Footer = () => {
     <footer className={styles.footer}>
       <div className={styles.footer_icons}>
 
-        <a href="https://www.facebook.com/profile.php?id=100004163760758" target="_blank" rel="noopener noreferrer">
+        <a className={styles.faFacebook} href="https://www.facebook.com/profile.php?id=100004163760758" target="_blank" rel="noopener noreferrer">
           <FontAwesomeIcon icon={faFacebook} size="2x" />
         </a>
 
-        <a href="https://www.instagram.com/murobedianidze/" target="_blank" rel="noopener noreferrer">
+        <a className={styles.faInstagram} href="https://www.instagram.com/murobedianidze/" target="_blank" rel="noopener noreferrer">
           <FontAwesomeIcon icon={faInstagram} size="2x" />
         </a>
-        <a href="mailto:bedianidze.muriko@gmail.com">
+        <a className={styles.faGoogle} href="mailto:bedianidze.muriko@gmail.com">
           <FontAwesomeIcon icon={faGoogle} size="2x" />
         </a>
       </div>
